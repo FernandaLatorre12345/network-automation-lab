fa543f63bf6d01bbd9f83413f673057278251bda
FernandaLatorre12345 <fernandalatorre.09@gmail.com>
2026-03-10 23:53:58 -0300

    frontend.py

--- frontend.py
+++ frontend.py
@@ -8,13 +8,19 @@ def run_config():
     host = host_entry.get()
     username = username_entry.get()
     password = password_entry.get()
+    hostname = hostname_entry.get()
     vlan_id = vlan_entry.get()
     vlan_name = name_entry.get()
 
     try:
 
-        hostname, vlan, backup = configure_switch(
-            host, username, password, vlan_id, vlan_name
+        backup = configure_switch(
+            host,
+            username,
+            password,
+            vlan_id,
+            vlan_name,
+            hostname
         )
 
         messagebox.showinfo(
@@ -30,26 +36,37 @@ def run_config():
 root = tk.Tk()
 root.title("Network Automation Tool")
 
+
 tk.Label(root, text="Device IP").grid(row=0, column=0)
 host_entry = tk.Entry(root)
 host_entry.grid(row=0, column=1)
+
 
 tk.Label(root, text="Username").grid(row=1, column=0)
 username_entry = tk.Entry(root)
 username_entry.grid(row=1, column=1)
 
+
 tk.Label(root, text="Password").grid(row=2, column=0)
 password_entry = tk.Entry(root, show="*")
 password_entry.grid(row=2, column=1)
 
-tk.Label(root, text="VLAN ID").grid(row=3, column=0)
+
+tk.Label(root, text="New Hostname").grid(row=3, column=0)
+hostname_entry = tk.Entry(root)
+hostname_entry.grid(row=3, column=1)
+
+
+tk.Label(root, text="VLAN ID").grid(row=4, column=0)
 vlan_entry = tk.Entry(root)
-vlan_entry.grid(row=3, column=1)
+vlan_entry.grid(row=4, column=1)
 
-tk.Label(root, text="VLAN Name").grid(row=4, column=0)
+
+tk.Label(root, text="VLAN Name").grid(row=5, column=0)
 name_entry = tk.Entry(root)
-name_entry.grid(row=4, column=1)
+name_entry.grid(row=5, column=1)
 
-tk.Button(root, text="Configure VLAN", command=run_config).grid(row=5, column=1)
+
+tk.Button(root, text="Configure Switch", command=run_config).grid(row=6, column=1)
 
 root.mainloop()
